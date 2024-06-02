945762b63c098d1894cd6b1bb68927475d87bb4d
ezequiel <ezequielfleitas040@gmail.com>
2024-06-02 10:13:28 -0300

    dockerized

--- src/app/servicios/services.service.ts
+++ src/app/servicios/services.service.ts
@@ -10,37 +10,38 @@ export class ServicesService {
 
   constructor(private http:HttpClient, private autenticacionServicio:AuthService ) { }
 
-  direccion:String = 'https://springboot-api-production-c063.up.railway.app/api'; //./assets/json/dataTest.json
+  // direccion:String = 'https://springboot-api-production-c063.up.railway.app/api';
+  direccion:String = 'http://localhost:8080/api'; // Direccion de testing
 
   obtenerDatos():Observable<any>{
     
     return this.http.get(this.direccion+'/ver/datos');
   }
 
-  guardarDatos( dir:String, datos:any ):Observable<any>{
+  guardarDatos( dir:string, datos:any ):Observable<any>{
     const token = this.autenticacionServicio.UsuarioAutenticado;
     const header = new HttpHeaders({
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`
     })
-    return this.http.post( 'https://springboot-api-production-c063.up.railway.app/api' + dir, datos, { headers:header });
+    return this.http.post( this.direccion.concat(dir) , datos, { headers:header });
   }
 
-  actualizarDatos(dir:String, datos:any):Observable<any>{
+  actualizarDatos(dir:string, datos:any):Observable<any>{
     const token = this.autenticacionServicio.UsuarioAutenticado;
     const header = new HttpHeaders({
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`
     })
-    return this.http.put( 'https://springboot-api-production-c063.up.railway.app/api' + dir, datos, { headers:header });
+    return this.http.put( this.direccion.concat(dir) + dir, datos, { headers:header });
   }
 
-  eliminarDatos(dir:String):Observable<any>{
+  eliminarDatos(dir:string):Observable<any>{
     const token = this.autenticacionServicio.UsuarioAutenticado;
     const header = new HttpHeaders({
       'Content-Type' : 'application/json',
       Authorization : `Bearer ${token}`
     })
-    return this.http.delete('https://springboot-api-production-c063.up.railway.app/api'+ dir, { headers:header });
+    return this.http.delete(this.direccion.concat(dir), { headers:header });
   }
 }
